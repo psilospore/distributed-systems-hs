23f7a2964b4a6d203b40ea1bd113010de152e326
Syed Jafri <syedjafri99@gmail.com>
2023-01-14 12:19:02 -0500

    Add rest of transtions:

--- src/LeaderElection.hs
+++ src/LeaderElection.hs
@@ -5,11 +5,25 @@ module LeaderElection where
 -- https://web.stanford.edu/~ouster/cgi-bin/cs190-winter20/lecture.php?topic=raft
 -- https://en.wikipedia.org/wiki/Raft_(algorithm)
 
-data ServerState = Leader | Follower | Candidate deriving (Show, Eq)
+data ServerState
+    = Leader
+    | Follower
+    | Candidate
+    deriving (Show, Eq)
 
-data Event = HeartbeatTimedOut | ReceivedMajorityVote | DiscoveredNewTerm deriving (Show, Eq)
+data Event
+    = HeartbeatTimedOut
+    | ReceivedMajorityVote
+    | DiscoveredNewTerm
+    | DiscoveredCurrentLeader
+    deriving (Show, Eq)
 
-data Action = StartLeaderProcess | StartFollowerProcess | NoOp deriving (Show, Eq)
+data Action
+    = StartLeaderProcess
+    | StartFollowerProcess
+    | StartElection
+    | NoOp
+    deriving (Show, Eq)
 
 data Config = Config
     { heartbeatTimeout :: Int
@@ -22,14 +36,22 @@ startup = Follower
 
 transition :: TransitionServerState
 transition stateWithEvent = case stateWithEvent of
-    (Follower, HeartbeatTimedOut) -> (Candidate, undefined)
+    -- Leader might be dead since the heartbeat timedout. Start election.
+    (Follower, HeartbeatTimedOut) -> (Candidate, StartElection)
     (Candidate, HeartbeatTimedOut) -> (Candidate, undefined)
     (Candidate, ReceivedMajorityVote) -> (Leader, undefined)
+    (Candidate, DiscoveredNewTerm) -> (Follower, StartFollowerProcess)
+    (Candidate, DiscoveredCurrentLeader) -> (Follower, StartFollowerProcess)
     (Leader, DiscoveredNewTerm) -> (Follower, StartFollowerProcess)
-    _ -> undefined
+    _ -> undefined -- TODO indicate invalid transition
 
 followerProcess :: IO ()
 followerProcess = undefined
 
 leaderProcess :: IO ()
 leaderProcess = undefined
+
+-- Vote for self
+-- Increment current election term
+startElection :: IO ()
+startElection = undefined
